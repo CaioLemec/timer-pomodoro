431ff29dde389b0a511849ea9480bc60a239d297
Caio Lemec <caiolemec@gmail.com>
2023-11-27 10:33:22 -0300

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './@types/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>hellow</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
